7c791e5377504c0ddc59691c7b48ebd038f9892a
Douglas Costa <dodop3000@gmail.com>
2012-05-31 13:05:45 -0300

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SiconSite::Application.config.secret_token = '873a41ce330332fde0d0956d99c8b2e40dde4e17cb4b648e7ea6fd0ca904520c7856ea2d55e0b75da0f3feeb80b5c4590ad8decfcd965f0df3741ca4edb9af47'
