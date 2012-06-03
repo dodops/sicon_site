9f6251f3cca19feaefd49815bed6f189a045a81d
Douglas Costa <dodop3000@gmail.com>
2012-06-03 13:27:15 -0300

    aumentando o número de caracters máximo no model

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,6 +3,6 @@ class Member < ActiveRecord::Base
 
   validates_presence_of :name, :bio, :email
 
-  validates_length_of :name, :within => 3..10
+  validates_length_of :name, :within => 3..20
   validates_format_of :email, :with => /^[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}$/i
 end
